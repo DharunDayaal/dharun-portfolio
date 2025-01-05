a58be433be261fc6d666e1a60912f52ccc2a92f2
dharun <dharundayaal@gmail.com>
2025-01-05 22:48:20 +0530

    Added projects image

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import project01 from "../assets/project01.png";
-import project02 from "../assets/project02.png"
+import project02 from "../assets/project02.png";
+import project03 from "../assets/project03.jpg";
+import project04 from "../assets/project04.png";
+import project05 from "../assets/project05.png";
+import project06 from "../assets/project06.png";
+import project07 from "../assets/project07.png";
 import "./projectpage.css";
 
 const ProjectPage = () => {
@@ -25,64 +30,64 @@ const ProjectPage = () => {
       id: 2,
       name: "Calculator App",
       description:
-        "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result",
-      image: project02,
-      technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS"],
+        "This project replicates the Apple Calculator app using Android Studio. It utilizes ConstraintLayout for a responsive design and implements built-in functions to handle calculations, demonstrating proficiency in Android development and functional UI design.",
+      image: project03,
+      technologies: ["Android Studio", "Constraint Layout"],
       githublink: "https://github.com/DharunDayaal/CalculatorApp",
     },
     {
       id: 3,
       name: "Spotknack Rewards Platform",
       description:
-        "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result",
+        "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result.",
       image: project02,
       technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS"],
-      githublink: "https://github.com",
+      githublink: "https://platform.spotknack.com",
     },
     {
       id: 4,
       name: "Swiggy Landing Page",
       description:
-        "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result",
-      image: project02,
-      technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS"],
-      githublink: "https://github.com",
+        "This project replicates Swiggy's landing page using React.js and Bootstrap. It features a responsive design, dynamic food category carousel, and restaurant grid layout, showcasing skills in component-based development and modern UI design techniques.",
+      image: project04,
+      technologies: ["React", "Bootstrap", "JSON", "HTML", "CSS"],
+      githublink: "https://github.com/DharunDayaal/swiggy-react",
     },
     {
       id: 5,
       name: "To-do App",
       description:
-        "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result",
-      image: project02,
-      technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS"],
-      githublink: "https://github.com",
+        "This project is a To-Do app built with React, React Modals, and Bootstrap. It allows users to add tasks, mark them as complete or incomplete, and edit or modify tasks, ensuring an interactive and user-friendly experience.",
+      image: project05,
+      technologies: ["React", "Bootstrap", "Modals", "HTML", "CSS"],
+      githublink: "https://github.com/DharunDayaal/todo-app",
     },
     {
       id: 6,
       name: "Weather App",
       description:
-        "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result",
-      image: project02,
-      technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS"],
-      githublink: "https://github.com",
+        "This project is a Weather App built using React and Axios. It integrates a Weather API to fetch real-time weather data for any location. The app displays temperature, weather conditions, and more in a user-friendly interface, demonstrating skills in API integration, React development, and creating dynamic, responsive applications.",
+      image: project06,
+      technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS", "Weather API"],
+      githublink: "https://github.com/DharunDayaal/weather-app",
     },
-    {
-      id: 7,
-      name: "Blog App",
-      description:
-        "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result",
-      image: project02,
-      technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS"],
-      githublink: "https://github.com",
-    },
+    // {
+    //   id: 7,
+    //   name: "Blog App",
+    //   description:
+    //     "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result",
+    //   image: project02,
+    //   technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS", "Firebase Authentication", "Realtime Firebase"],
+    //   githublink: "https://github.com",
+    // },
     {
       id: 8,
       name: "StudioMPLS Clone Website",
       description:
-        "The Rewards Platform is a real-time project from Spotknack, where the mentees can submit their task, blogs, tutorials, linkedin posts and projects which will be evaluated by their mentors and the platform will allocate the coins based on the evaluation result",
-      image: project02,
-      technologies: ["React", "Bootstrap", "Axios", "HTML", "CSS"],
-      githublink: "https://github.com",
+        "The StudioMPLS Clone Website is a responsive web design built using HTML, CSS, JavaScript, and Bootstrap. It mimics the features of the StudioMPLS website, showcasing modern design elements, a user-friendly interface, and mobile-friendly layout.",
+      image: project07,
+      technologies: ["HTML", "CSS", "Javascript", "Bootstrap"],
+      githublink: "https://github.com/DharunDayaal/studiompls-clone-website",
     },
   ];
 
